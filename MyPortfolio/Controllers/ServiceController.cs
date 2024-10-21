a598563bbd5486f92141798ba52572ae7dc04ffb
Öznur <oznur.sgt27@gmail.com>
2024-10-21 18:25:15 +0300

    master

---
+++ MyPortfolio/Controllers/ServiceController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyPortfolio.Models;
+
+namespace MyPortfolio.Controllers
+{
+    public class ServiceController : Controller
+    {
+        MyPortfolioDbEntities db =new MyPortfolioDbEntities();
+        public ActionResult ServiceIndex()
+        {
+            var value= db.Service.ToList();
+            return View(value);
+        }
+        public ActionResult CreateService()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyPortfolio/Controllers/SkillController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolio.Controllers
+{
+    public class SkillController : Controller
+    {
+        // GET: Skill
+        public ActionResult SkillIndex()
+        {
+            return View();
+        }
+    }
+}

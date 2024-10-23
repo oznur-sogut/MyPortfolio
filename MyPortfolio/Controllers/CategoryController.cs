86684c72ed7a4cca70507615431eb61d8747477b
Öznur <oznur.sgt27@gmail.com>
2024-10-23 10:57:16 +0300

    master

---
+++ MyPortfolio/Controllers/CategoryController.cs
@@ -0,0 +1,53 @@
+﻿using MyPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolio.Controllers
+{
+    public class CategoryController : Controller
+    {
+        MyPortfolioDbEntities db = new MyPortfolioDbEntities();
+        public ActionResult CategoryIndex()
+        {
+            var value= db.Category.ToList();
+            return View(value);
+        }
+        public ActionResult DeleteCategory(int id)
+        {
+            var value=db.Category.Find(id);
+            db.Category.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("CategoryIndex");
+        }
+        [HttpGet]
+        public ActionResult UpdateCategory( int id)
+        {
+            var value = db.Category.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateCategory(Category category)
+        {
+            var value = db.Category.Find(category.CategoryID);
+            value.CategoryID = category.CategoryID;
+            value.CategoryName = category.CategoryName;
+            db.SaveChanges();
+            return RedirectToAction("CategoryIndex");
+        }
+        [HttpGet]
+        public ActionResult CreateCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateCategory(Category category)
+        {
+            db.Category.Add(category);
+            db.SaveChanges();
+            return RedirectToAction("CategoryIndex");
+        }
+    }
+}
--- MyPortfolio/Controllers/TestimonialController.cs
+++ MyPortfolio/Controllers/TestimonialController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MyPortfolio.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,14 +11,49 @@ namespace MyPortfolio.Controllers
 {
     public class TestimonialController : Controller
     {
-        // GET: Testimonial
-        public ActionResult Index()
+        MyPortfolioDbEntities db= new MyPortfolioDbEntities();
+        public ActionResult TestimonialIndex()
         {
-            return View();
+            var value = db.Testimonial.ToList();
+            return View(value);
         }
+        [HttpGet]
         public ActionResult CreateTestimonial()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult CreateTestimonial(Testimonial testimonial)
+        {
+            db.Testimonial.Add(testimonial);
+            db.SaveChanges();
+            return RedirectToAction("TestimonialIndex");
+        }
+		// id nin zorunluluğu routConfigten gelir
+		public ActionResult DeleteTestimonial(int id)
+        {
+            var value = db.Testimonial.Find(id);
+            db.Testimonial.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("TestimonialIndex");
+
+        }
+        [HttpGet]
+        public ActionResult UpdateTestimonial(int id)
+        {
+            var value= db.Testimonial.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateTestimonial (Testimonial testimonial)
+        {
+            var value= db.Testimonial.Find(testimonial.TestimonialID);
+            value.TestimonialNameSurname= testimonial.TestimonialNameSurname;
+            value.TestimonialTitle=testimonial.TestimonialTitle;
+            value.TestimonialImageUrl=testimonial.TestimonialImageUrl;
+            value.TestimonialComment=testimonial.TestimonialComment;
+            db.SaveChanges();
+            return RedirectToAction("TestimonialIndex");
         }
     }
 }

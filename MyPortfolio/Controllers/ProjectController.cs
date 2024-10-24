d538792f35a3f9a1b0f4543459d9cfc7e1fdbf53
Öznur <oznur.sgt27@gmail.com>
2024-10-24 12:56:14 +0300

    master

--- MyPortfolio/Controllers/ProjectController.cs
+++ MyPortfolio/Controllers/ProjectController.cs
@@ -16,5 +16,58 @@ namespace MyPortfolio.Controllers
             var value= db.Project.ToList();
             return View(value);
         }
+        public ActionResult DeleteProject(int id)
+        {
+            var value = db.Project.Find(id);
+            db.Project.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("ProjectIndex");
+        }
+        [HttpGet]
+        public ActionResult UpdateProject(int id)
+        {
+			List<SelectListItem> values = (from i in db.Category.ToList()
+										  select new SelectListItem
+										  {
+											  Text = i.CategoryName,
+											  Value = i.CategoryID.ToString()
+										  }).ToList();
+			ViewBag.Value = values;
+			var value = db.Project.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateProject(Project project)
+        {
+            var value = db.Project.Find(project.ProjectID);
+            value.ProjectTitle = project.ProjectTitle;
+            value.ProjectDescription = project.ProjectDescription;
+            var values = db.Category.Where(x=>x.CategoryID==project.Category.CategoryID).FirstOrDefault();
+                value.ProjectCategory=values.CategoryID;
+           
+            db.SaveChanges();
+            return RedirectToAction("ProjectIndex");
+        }
+        [HttpGet]
+        public ActionResult CreateProject()
+        {
+			List<SelectListItem> value = (from i in db.Category.ToList()
+										  select new SelectListItem
+										  {
+											  Text = i.CategoryName,
+											  Value = i.CategoryID.ToString()
+										  }).ToList();
+            ViewBag.values=value;
+			return View();
+        }
+        [HttpPost]
+        public ActionResult CreateProject(Project project)
+        {
+            var value = db.Category.Where(x=>x.CategoryID==project.Category.CategoryID).FirstOrDefault();
+            project.Category = value;
+            db.Project.Add(project);
+            db.SaveChanges();
+            return RedirectToAction("ProjectIndex");
+        }
     }
 }
